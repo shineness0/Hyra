44189627f231bcf0f0f6e85fa231df050678cc17
Sunday <shineness00@gmail.com>
2024-08-07 01:27:31 -0700

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,19 @@ const Footer = () => {
     <div>
       <div className="bg-black">
         <div className="flex items-center justify-center w-1/6 p-4 mx-auto">
-          <Link href="https://www.linkedin.com/in/vee-jay/" className="mx-2">
+          <Link href="" className="mx-2">
             <FaLinkedin
               size={25}
               className="text-[#71b7fb] hover:opacity-80 transition"
             />
           </Link>
-          <Link href="https://twitter.com/bytes_nova" className="mx-2">
+          <Link href="" className="mx-2">
             <FaTwitter
               size={25}
               className="text-[#0866ff] hover:opacity-80 transition"
             />
           </Link>
-          <Link
-            href="https://web.facebook.com/victorjohn.aderibigbe.7"
-            className="mx-2"
-          >
+          <Link href="" className="mx-2">
             <FaFacebook
               size={25}
               className="text-[#1d9bf0] hover:opacity-80 transition"
@@ -33,7 +30,7 @@ const Footer = () => {
       </div>
       <div className="p-4 text-xs text-white bg-primary-200 md:text-base">
         <span className="flex items-center justify-center">
-          &copy;2024 Victor Aderibigbe | BytesNova&#8482;
+          &copy;2024 Olaniyi Sunday | Olasun Technology&#8482;
         </span>
       </div>
     </div>
